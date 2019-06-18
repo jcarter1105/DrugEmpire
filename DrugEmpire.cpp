872842d2ee9bc8df75f06d1625e5d47119f65c22
Myles Charlesworth <mylesc2016@gmail.com>
2019-06-18 18:38:47 -0400

    Setup V2

--- DrugEmpire.cpp
+++ DrugEmpire.cpp
@@ -1,8 +1,9 @@
 //
 //  Drug Empire
 //
-//  Created by James Carter on 6/16/19.
+//  Created by James Carter and Myles Charlesworth on 6/18/19.
 //  Copyright © 2019 James Carter. All rights reserved.
+//  
 //
 #include <iostream>
 #include <string>
